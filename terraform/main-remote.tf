7e91d4cca4c08f7cdb01b837bef4361fcb9f5a21
Alexey Markov <yi-7777@rambler.ru>
2022-04-23 17:19:00 +0000

    HW 7.4

---
+++ terraform/main-remote.tf
@@ -0,0 +1,54 @@
+provider "yandex" {
+  token = var.YC_TOKEN
+  cloud_id  = var.YC_CLOUD_ID
+  folder_id = var.YC_FOLDER_ID
+  zone      = "ru-central1-a"
+  }
+
+locals { 
+  vm_instans_name_platform = { 
+    stage = { 
+      "vm02" = { platform = "standard-v1", core_fraction = 5, cores = 2, memory = 2, size = 20 } 
+     } 
+    prod = {
+    "vm02" = { platform = "standard-v2", core_fraction = 10, cores = 2, memory = 2, size = 40 } 
+    "vm03" = { platform = "standard-v2", core_fraction = 10, cores = 2, memory = 2, size = 40 }
+    } 
+  } 
+}
+
+resource "yandex_vpc_network" "net" {
+  name = "net" 
+} 
+
+resource "yandex_vpc_subnet" "subnet" {
+  v4_cidr_blocks = ["192.168.1.0/24"]
+  zone = "ru-central1-a"
+  network_id = "${yandex_vpc_network.net.id}"
+  name = "subnet"
+}
+
+resource "yandex_compute_instance" "vm_app" {
+  for_each = local.vm_instans_name_platform[terraform.workspace]
+  name = each.key
+  resources {
+    cores  = each.value.cores
+    memory = each.value.memory
+    core_fraction  = each.value.core_fraction
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd879gb88170to70d38a"
+      size = each.value.size
+    }
+  }
+
+  network_interface {
+    subnet_id = "${yandex_vpc_subnet.subnet.id}"
+ } 
+
+  metadata = {
+    ssh-keys = "${file("~/.ssh/id_rsa.pub")}"
+  }
+}
---
+++ terraform/variable.tf
@@ -0,0 +1,20 @@
+variable "YC_TOKEN" {
+  default = ""
+}
+
+variable "YC_CLOUD_ID" {
+  default = ""
+}
+
+
+variable "YC_FOLDER_ID" {
+  default = ""
+}
+
+variable "yc_secret_key" {
+  default = ""
+}
+
+variable "yc_access_key" {
+  default = ""
+}
